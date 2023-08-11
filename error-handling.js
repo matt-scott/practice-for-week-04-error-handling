8d8625b48c2d07a1390574523396086943574954
Matt Scott <mattscott251@gmail.com>
2023-08-11 11:40:05 -0500

    completed all problems

--- error-handling.js
+++ error-handling.js
@@ -7,13 +7,38 @@ function sum(array) {
   return sum;
 }
 
-let res = sum(null);
-console.log(res);
+try {
+  let res = sum(null);
+  console.log(res);
+}
+catch (error) {
+  if (error instanceof TypeError) {
+    console.error(`Wrong type: ${error.message}`);
+  }
+  else {
+    console.error(error.message);
+  }
+}
+
 
 // 2.
+function sayName(name) {
+  if (typeof name === 'string') {
+    console.log(name);
+  }
+  else {
+    throw new TypeError('Invalid Name! Must be a string!');
+  }
+}
 // tests
-sayName("Alex");
-sayName(1);
+try {
+  sayName("Alex");
+  sayName(1);
+}
+catch (error) {
+  console.error(error.message);
+}
+
 // Your code here
 
 // 3.
@@ -23,4 +48,12 @@ function greet(greeting) {
   }
 
   console.log(greeting);
+}
+
+try {
+  greet('Hi! This was a success!')
+  greet(null);
+}
+catch (error) {
+  console.log('Hello World!')
 }
